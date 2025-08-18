76ea9fce0b4d2b44cc041d064e110f79fe2f7771
MoMaher2004 <mohamed.ahmedmaher217@gmail.com>
2025-08-18 15:26:22 +0300

    server.js updated

--- api/server.js
+++ api/server.js
@@ -7,7 +7,11 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
+  methods: ["GET", "POST", "PATCH", 'PUT', "DELETE"],
+  credentials: true
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
