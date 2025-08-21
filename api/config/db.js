2f7ac65d2fcf263b5d339d80f062bc8c385b7232
MoMaher2004 <mohamed.ahmedmaher217@gmail.com>
2025-08-21 19:24:21 +0300

    db.js now converts the float numbers to numbers automatically instead of retrieving it as strings

--- api/config/db.js
+++ api/config/db.js
@@ -6,6 +6,13 @@ const pool = mysql.createPool({
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   timezone: '+00:00',
+  typeCast: function (field, next) {
+    if (field.type === 'NEWDECIMAL' || field.type === 'DECIMAL') {
+      const val = field.string()
+      return val === null ? null : Number(val)
+    }
+    return next()
+  }
 })
 
 module.exports = pool
