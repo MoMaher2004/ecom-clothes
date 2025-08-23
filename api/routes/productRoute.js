496ab563cca5d0fc9756fa20d3c9907d8d9e7e1e
MoMaher2004 <mohamed.ahmedmaher217@gmail.com>
2025-08-23 11:20:11 +0300

    a litle bit of cleaner code

--- api/routes/productRoute.js
+++ api/routes/productRoute.js
@@ -35,7 +35,7 @@ router.patch('/editProduct/:id', verifyToken, adminOnly, editProduct)
 router.delete('/deleteProduct/:id', verifyToken, adminOnly, deleteProduct)
 router.patch('/restoreProduct/', verifyToken, adminOnly, restoreProduct)
 
-router.post('/uploadImage', verifyToken, adminOnly, imagesUtils.uploadImages.array('images', 10), uploadImages)
+router.post('/uploadImages', verifyToken, adminOnly, imagesUtils.uploadImages.array('images', 10), uploadImages)
 router.delete('/deleteImage/:imageName', verifyToken, adminOnly, deleteImage)
 
 module.exports = router
